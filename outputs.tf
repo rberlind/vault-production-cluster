36391022769da2fde79696a4ca0efb1bc959893f
rberlind <roger@hashicorp.com>
2018-10-01 09:59:29 -0400

    added instance profile name output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-// Can be used to add additional SG rules to Vault instances.
+// Vault Security Group ID
 output "vault_security_group" {
-    value = "${aws_security_group.vault.id}"
+  value = "${aws_security_group.vault.id}"
 }
 
-// Can be used to add additional SG rules to the Vault ELB.
+// ELB Security Group ID
 output "vault_elb_security_group" {
-    value = "${aws_security_group.vault_elb.id}"
+  value = "${aws_security_group.vault_elb.id}"
+}
+
+// IAM Instance Profile Name
+output "iam_profile_name" {
+  value = "${aws_iam_instance_profile.instance_profile.name}"
 }
